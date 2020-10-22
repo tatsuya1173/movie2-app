75bf9f11562651f7259d2ad8b97307ae5a54288c
sando0710 <54323405+sando0710@users.noreply.github.com>
2020-10-22 18:58:21 +0900

    カラム修正down

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -28,7 +28,6 @@ end
   end
 
   def create
-
     @movie = Movie.new(movie_params)
     @movie.user_id = current_user.id
     if @movie.save
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,27 @@
 
 ActiveRecord::Schema.define(version: 2020_08_29_035840) do
 
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment"
     t.bigint "user_id"
@@ -42,10 +63,9 @@ ActiveRecord::Schema.define(version: 2020_08_29_035840) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "movies"
   add_foreign_key "comments", "users"
   add_foreign_key "movies", "users"
